12499da4b18c49a5f21f99158f84ad7219f9e393
hi-ashleyj <ajor023@gmail.com>
2021-03-28 22:08:27 +1300

    add loading from file, patch never-ending loops, and prepare for allow/deny logic

--- app.js
+++ app.js
@@ -15,15 +15,47 @@ for (let i = 0; i < process.argv.length; i++) {
 let doGitHubAccept = async function() {
     // Retrieve the Personal Access Token from config.json
     let access_token = "";
+    let strictMode = false;
+    let allowList = [];
+    let denyList = [];
     if (passed_token) {
         access_token = passed_token;
-    } else {
-        try {
-            access_token = JSON.parse(await fs.readFile(path.resolve(__dirname, "config.json"), { encoding: 'utf8' })).access_token;
-        } catch (_err) {
+    } 
+
+    // Try to load the config info
+    try {
+        config = JSON.parse(await fs.readFile(path.resolve(__dirname, "config.json"), { encoding: 'utf8' }));
+
+        // Load access token from file if no single-use one was specified
+        if (config.access_token && access_token.length <= 0) {
+            access_token = config.access_token;
+        }
+
+        // Allow "strict mode" (only people added to a list are added)
+        if (config.strict) {
+            strictMode = true;
+        }
+
+        // Load in allowed users
+        if (config.allow) {
+            if (config.allow instanceof Array) { // Allow for this to load in from file/folder in future. This should be an array
+                allowList = config.allow;
+            }
+        }
+
+        // Load in denied users
+        if (config.deny) {
+            if (config.deny instanceof Array) { // Allow for this to load in from file/folder in future. This should be an array
+                denyList = config.deny;
+            }
+        }
+    } catch (_err) {
+        // If there is no access token at all
+        if (access_token.length <= 0) {
             console.log("Error getting your access token: please check that config.json exists and is valid JSON");
             return;
         }
+        // else fail read silently
     }
     
     if (access_token.length < 1) {
@@ -37,15 +69,25 @@ let doGitHubAccept = async function() {
 
     // Global definition for loop persistence
     let lastRequestHits = 0;
+
+    // And then ready up for non-changes
+    let lastResponse = "";
     
     do {
         let results = [];
         
         try {
             // Attempt to get list of all invitations and parse the response as JSON
-            results = JSON.parse(await uti.hGet("https://api.github.com/user/repository_invitations", headers));
+            let unJSON = await uti.hGet("https://api.github.com/user/repository_invitations", headers);
+            if (lastResponse == unJSON) {
+                console.log("Response unchanged. Breaking loop.");
+                break;
+            }
+            lastResponse = unJSON;
+            results = JSON.parse(unJSON);
         } catch (_err) {
             console.log("Error getting a batch of invitations");
+            lastRequestHits = 0;
         }
 
         // set for reference later
@@ -53,7 +95,11 @@ let doGitHubAccept = async function() {
         console.log("Got batch of " + lastRequestHits + " request" + ((lastRequestHits == 1) ? "": "s") + " to accept");
 
         for (let res of results) {
+            let verdict = !strictMode;
             let invitationID = res.id;
+
+
+
 
             // Send PATCH request to accept invitation
             try {
@@ -62,6 +108,15 @@ let doGitHubAccept = async function() {
             } catch (_err) {
                 console.log("Error accepting invite #" + invitationID);
             }
+
+            // Send DELETE request to deny invitation
+
+            // try {
+            //     await uti.hDelete("https://api.github.com/user/repository_invitations/" + invitationID, headers);
+            //     console.log("Denied invite #" + invitationID);
+            // } catch (_err) {
+            //     console.log("Error denying invite #" + invitationID);
+            // }
             
         }
 
